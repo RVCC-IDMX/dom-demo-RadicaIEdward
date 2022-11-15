ba3f7f60d26cef6f47fecdf9699bfda48d98276c
RadicaIEdward <edward.o.galindez@gmail.com>
2022-11-15 13:10:09 -0500

    Select elements

--- main.js
+++ main.js
@@ -5,4 +5,13 @@ JavaScript Crash Course For Beginners
 https://youtu.be/hdI2bqOjy3c?t=4228 
 
 Start at 1:10:29
+
  */
+
+//Single element
+//console.log(document.getElementById("my-form"));
+//console.log(document.querySelector("h1"));
+
+// Multiple element
+//console.log(document.querySelectorAll('.item));
+//console.log(getElementByClassName('item'));
