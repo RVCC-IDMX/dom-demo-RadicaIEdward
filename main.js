2d60ed9511544cbf03e1caa128d1526b9cd1d6fa
RadicaIEdward <edward.o.galindez@gmail.com>
2022-11-15 13:14:08 -0500

    Complete selections

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ Video by Brad Traversy
 JavaScript Crash Course For Beginners
 https://youtu.be/hdI2bqOjy3c?t=4228 
 
+Single element
+console.log(document.getElementById("my-form"));
+console.log(document.querySelector("h1"));
+
+Multiple elements
+console.log(document.querySelectorAll('.item));
+console.log(document.getElementByClassName('item'));
+console.log(document.getElementByClassName('li'));
+
+const items = document.querySelectorAll("item");
+
+items.forEach((item) => console.log(item));
 Start at 1:10:29
 
  */
-
-//Single element
-//console.log(document.getElementById("my-form"));
-//console.log(document.querySelector("h1"));
-
-// Multiple element
-//console.log(document.querySelectorAll('.item));
-//console.log(getElementByClassName('item'));
