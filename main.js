89055f809c5de93ca8ca7688048ea29984179ed9
RadicaIEdward <edward.o.galindez@gmail.com>
2022-11-15 13:27:20 -0500

    Complete events

--- main.js
+++ main.js
@@ -32,6 +32,20 @@ ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
 const btn = document.querySelector('.btn');
 btn.style.background = 'red';
 
- */
-
+EVENTS:
 const btn = document.querySelector(".btn");
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(e.target.className);
+  //console.log(e.target.id);
+});
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('#my-form').style.background = #ccc;
+  document.querySelector('body').classList.add('bg-dark');
+  document.querySelector('.items').lastElementChild.innerHTML = <h1>Hello</h1>;
+});
+
+ */
